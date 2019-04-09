9a075b078e52dc04b392ec7676635933f8576f62
Scott Wentz <scott.wentz@epitech.eu>
2019-04-09 17:17:26 -0400

    empty fields check for add vendor view

--- src/userinterface/AddInventoryView.java
+++ src/userinterface/AddInventoryView.java
@@ -11,7 +11,6 @@ import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -24,46 +23,34 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
-
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import model.Vendor;
 
 import java.util.Properties;
-import java.util.regex.Pattern;
 
-import javafx.util.StringConverter;
-import model.InventoryItem;
-import model.InventoryItemType;
 // project imports
 import impresario.IModel;
 
-public class AddInventoryView extends View{
-	// GUI components
-	//Restaurant owner provides the Inventory Manager 
-	//with the Item Type Name, Units, Unit Measure, Validity Days, 
-	//Reorder Point, and Notes of the Inventory Item Type
-	// GUI components
-	protected TextField TypeName;
-	protected TextField Units;
-	protected TextField UnitMeasure;
-	protected TextField ValidityDay;
-	protected TextField RoerderPoint;
-	protected TextField Notes;
-	protected TextField ItemType;
-	protected ComboBox Status;
-	
-	protected TextField serviceCharge;
+/** The class containing the Account View  for the ATM application */
+//==============================================================
+public class AddVendorView extends View
+{
 
+	// GUI components
+	protected TextField nameField;
+	protected TextField phoneNumberField;
+	protected ComboBox statusBox;
+	protected TextField serviceCharge;
 
 	protected Button cancelButton, submitButton;
 
 	// For showing error message
 	protected MessageView statusLog;
+
 	// constructor for this class -- takes a model object
 	//----------------------------------------------------------
-	public AddInventoryView(IModel account)
+	public AddVendorView(IModel account)
 	{
-		super(account, "AddInventoryView");
+		super(account, "AddVendorView");
 
 		// create a container for showing the contents
 		VBox container = new VBox(10);
@@ -78,8 +65,6 @@ public class AddInventoryView extends View{
 		container.getChildren().add(createStatusLog("             "));
 
 		getChildren().add(container);
-
-		//populateFields();
 
 		myModel.subscribe("ServiceCharge", this);
 		myModel.subscribe("UpdateStatusMessage", this);
@@ -107,7 +92,7 @@ public class AddInventoryView extends View{
 	//-------------------------------------------------------------
 	private VBox createFormContent()
 	{
-		VBox vbox = new VBox(20);
+		VBox vbox = new VBox(10);
 
 		GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
@@ -115,74 +100,44 @@ public class AddInventoryView extends View{
         grid.setVgap(10);
         grid.setPadding(new Insets(25, 25, 25, 25));
         
-        Text prompt = new Text("ITEM INVENTORY INFORMATION");
+        Text prompt = new Text("VENDOR INFORMATION");
         prompt.setWrappingWidth(400);
         prompt.setTextAlignment(TextAlignment.CENTER);
         prompt.setFill(Color.BLACK);
         grid.add(prompt, 0, 0, 2, 1);
-        
-        Text itemTypeNameLabel = new Text(" Inventory Item Type Name : ");
+
+		Text vendorNameLabel = new Text(" Vendor Name : ");
 		Font myFont = Font.font("Helvetica", FontWeight.BOLD, 12);
-		itemTypeNameLabel.setFont(myFont);
-		itemTypeNameLabel.setWrappingWidth(150);
-		itemTypeNameLabel.setTextAlignment(TextAlignment.RIGHT);
-		grid.add(itemTypeNameLabel, 0, 1);
+		vendorNameLabel.setFont(myFont);
+		vendorNameLabel.setWrappingWidth(150);
+		vendorNameLabel.setTextAlignment(TextAlignment.RIGHT);
+		grid.add(vendorNameLabel, 0, 1);
 
-		TypeName = new TextField();
-		grid.add(TypeName, 1, 1);
+		nameField = new TextField();
+		grid.add(nameField, 1, 1);
 
-        Text inventoryUnits = new Text(" Units : ");
-		inventoryUnits.setFont(myFont);
-		inventoryUnits.setWrappingWidth(150);
-		inventoryUnits.setTextAlignment(TextAlignment.RIGHT);
-		grid.add(inventoryUnits, 0, 2);
+		Text phoneLabel = new Text(" Vendor Phone Number : ");
+		phoneLabel.setFont(myFont);
+		phoneLabel.setWrappingWidth(150);
+		phoneLabel.setTextAlignment(TextAlignment.RIGHT);
+		grid.add(phoneLabel, 0, 2);
 
-		Units = new TextField();
-		grid.add(Units, 1, 2);
+		phoneNumberField = new TextField();
+		grid.add(phoneNumberField, 1, 2);
 
-		Text inventoryUnitMeasure = new Text(" Unit measure : ");
-		inventoryUnitMeasure.setFont(myFont);
-		inventoryUnitMeasure.setWrappingWidth(150);
-		inventoryUnitMeasure.setTextAlignment(TextAlignment.RIGHT);
-		grid.add(inventoryUnitMeasure, 0, 3);
+		Text statusLabel = new Text(" Status : ");
+		statusLabel.setFont(myFont);
+		statusLabel.setWrappingWidth(150);
+		statusLabel.setTextAlignment(TextAlignment.RIGHT);
+		grid.add(statusLabel, 0, 3);
 
-		UnitMeasure = new TextField();
-		grid.add(UnitMeasure, 1, 3);
 		
-		Text inventoryValidityDay = new Text(" Validity day: ");
-		inventoryValidityDay.setFont(myFont);
-		inventoryValidityDay.setWrappingWidth(150);
-		inventoryValidityDay.setTextAlignment(TextAlignment.RIGHT);
-		grid.add(inventoryValidityDay, 0, 4);
-
-		ValidityDay = new TextField();
-		grid.add(ValidityDay, 1, 4);
-		
-		Text inventoryRoerderPoint = new Text(" Roerder point: ");
-		inventoryRoerderPoint.setFont(myFont);
-		inventoryRoerderPoint.setWrappingWidth(150);
-		inventoryRoerderPoint.setTextAlignment(TextAlignment.RIGHT);
-		grid.add(inventoryRoerderPoint, 0, 5);
-
-		RoerderPoint = new TextField();
-		grid.add(RoerderPoint, 1, 5);
-		
-		Text inventoryNotes = new Text(" Notes: ");
-		inventoryNotes.setFont(myFont);
-		inventoryNotes.setWrappingWidth(150);
-		inventoryNotes.setTextAlignment(TextAlignment.RIGHT);
-		grid.add(inventoryNotes, 0, 6);
-
-		Notes = new TextField();
-		grid.add(Notes, 1, 6);
-		
-		Status = new ComboBox();
-		Status.getItems().addAll(
+		statusBox = new ComboBox();
+		statusBox.getItems().addAll(
 				"Active",
 				"Inactive");
-		grid.add(Status, 1, 7);
-		Status.getSelectionModel().selectFirst();
-
+		grid.add(statusBox, 1, 3);
+		statusBox.getSelectionModel().selectFirst();
 
 		HBox doneCont = new HBox(10);
 		doneCont.setAlignment(Pos.CENTER);
@@ -200,14 +155,16 @@ public class AddInventoryView extends View{
 		submitButton = new Button("Submit");
 		submitButton.setFont(Font.font("Arial", FontWeight.BOLD, 14));
 		submitButton.setOnAction(new EventHandler<ActionEvent>() {
-
-  		     @Override
-  		     public void handle(ActionEvent e) {
-  		    	clearErrorMessage();
-  		    	addInventoryItem();
-  		     }
-       	  });
-		
+			 @Override
+		     public void handle(ActionEvent e) {
+			 	if (isSomethingEmpty()) {
+			 		displayMessage("Please fill out all the fields.");
+				} else {
+					clearErrorMessage();
+					addVendor();
+				}
+   	  }
+	});
 		doneCont.getChildren().add(cancelButton);
 		doneCont.getChildren().add(submitButton);
 	
@@ -217,24 +174,20 @@ public class AddInventoryView extends View{
 		return vbox;
 	}
 	
-	private void addInventoryItem() {
+	private void addVendor() {
+		// add the properties
+		Properties p2 = new Properties();
+		//p2.setProperty("Id", ""); // this field is auto incremented, dont touch
+		p2.setProperty("Name", nameField.getText());
+		p2.setProperty("PhoneNumber", phoneNumberField.getText());
+		p2.setProperty("Status", statusBox.getValue().toString());
 		
-		// create the properties of the inventoryIte,
-		Properties p1 = new Properties();
-			//p1.setProperty("InventoryItemId", "1"); // this is auto incremented dont touch
-			p1.setProperty("ItemTypeName", TypeName.getText());
-			p1.setProperty("Units", Units.getText());
-			p1.setProperty("UnitMeasure", UnitMeasure.getText());
-			p1.setProperty("ValidityDays", ValidityDay.getText());
-			p1.setProperty("ReorderPoint", RoerderPoint.getText());
-			p1.setProperty("Notes", Notes.getText());
-			p1.setProperty("Status", Status.getValue().toString());
-			// Create the Inventory Item object with the properties we made
-			InventoryItemType it = new InventoryItemType(p1);
-			// add it to the db
-			it.update();
+		// create item using properties, then add to db
+		Vendor ve = new Vendor(p2);
+		ve.update();
 	}
-	
+
+
 	// Create the status log field
 	//-------------------------------------------------------------
 	protected MessageView createStatusLog(String initialMessage)
@@ -243,7 +196,6 @@ public class AddInventoryView extends View{
 
 		return statusLog;
 	}
-
 
 	/**
 	 * Update method
@@ -259,6 +211,14 @@ public class AddInventoryView extends View{
 			serviceCharge.setText(val);
 			displayMessage("Service Charge Imposed: $ " + val);
 		}
+	}
+
+	private Boolean isSomethingEmpty() {
+		if (nameField.getText().isEmpty() || phoneNumberField.getText().isEmpty() || serviceCharge.getText().isEmpty() ||
+			statusBox.getSelectionModel().isEmpty() ) {
+			return true;
+		}
+		return false;
 	}
 
 	/**
@@ -287,4 +247,11 @@ public class AddInventoryView extends View{
 	{
 		statusLog.clearErrorMessage();
 	}
+
 }
+
+//---------------------------------------------------------------
+//	Revision History:
+//
+
+
